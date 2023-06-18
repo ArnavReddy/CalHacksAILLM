3a0b13b56f4b1054b558ed12e1b33c5fac4c2a4f
arnavr <arnavr@umich.edu>
2023-06-17 19:31:53 -0700

    add hume to client

--- src/components/LiveAssistant/LiveAssistant.tsx
+++ src/components/LiveAssistant/LiveAssistant.tsx
@@ -1,6 +1,8 @@
 import { useCallback, useEffect, useRef } from "react";
 import Webcam from "react-webcam";
+// import WebSocket from "ws";
 import {socket} from '../../socket';
+import { usingBrowserWebSocket } from "engine.io-client/build/esm/transports/websocket-constructor";
 
 const videoConstraints = {
   width: 1280,
@@ -8,24 +10,95 @@ const videoConstraints = {
   facingMode: "user"
 };
 
+
 const LiveAssistant = () => {
   const webcamRef = useRef(null);
+  const wsRef = useRef(null);
+
   const capture = useCallback(
     () => {
 
       if (webcamRef) {
         const imageSrc = webcamRef.current.getScreenshot();
-        // console.log(imageSrc);
+         console.log(imageSrc);
+         sendImageFacePayload(imageSrc);
+        // sendAudioProsodyPayload();
       }
     },
     [webcamRef]
   );
 
   useEffect(() => {
-    setInterval(capture, 1000);
+    // wsRef.current = new WebSocket("wss://api.hume.ai/v0/stream/models", {
+    //   headers: {
+    //     Authorization: "q7KqeFZxKy8uM3aDw0tgGnYQmXIrdC8de43cz5XKr0rrFpjq",
+    //     "X-Hume-Api-Key": "q7KqeFZxKy8uM3aDw0tgGnYQmXIrdC8de43cz5XKr0rrFpjq",
+    //   },
+    // });
+
+    // wsRef.current = new WebSocket("ws://api.hume.ai/v0/stream/models",
+    //   headers: {
+    //   Authorization: "q7KqeFZxKy8uM3aDw0tgGnYQmXIrdC8de43cz5XKr0rrFpjq",
+    //   "X-Hume-Api-Key": "q7KqeFZxKy8uM3aDw0tgGnYQmXIrdC8de43cz5XKr0rrFpjq",
+    // });
+
+    wsRef.current = new WebSocket("wss://api.hume.ai/v0/stream/models?apikey=q7KqeFZxKy8uM3aDw0tgGnYQmXIrdC8de43cz5XKr0rrFpjq");
+    // wsRef.current.headers = {
+    //   Authorization: "q7KqeFZxKy8uM3aDw0tgGnYQmXIrdC8de43cz5XKr0rrFpjq",
+    //   "X-Hume-Api-Key": "q7KqeFZxKy8uM3aDw0tgGnYQmXIrdC8de43cz5XKr0rrFpjq",
+    // }
+
+    
+    wsRef.current.onopen = () => {
+      console.log("WebSocket connection established");
+    };
+
+    wsRef.current.onclose = () => {
+      console.log("WebSocket connection closed");
+    };
+
+    wsRef.current.onerror = function (error) {
+      console.log(error.ErrorEvent);
+    };
+
+    wsRef.current.onmessage = function (event) {
+      console.log(`${event.data}`);
+    };
+
+
+    setInterval(capture, 10000);
     // example
     socket.connect();
   }, []);
+
+  const sendImageFacePayload = (base64Image) => {
+    const payload = {
+    models: {
+      face: {
+        facs: {},
+        descriptions: {},
+      },
+    },
+    data: base64Image,
+    };
+    
+  wsRef.current.send(JSON.stringify(payload));
+  }
+
+  const sendAudioProsodyPayload = (base64Audio) => {
+    const payload = {
+    models: {
+      prosody: {},
+    },
+    data: base64Audio,
+  };
+
+  wsRef.current.send(JSON.stringify(payload));
+  }
+
+  // const convertImageToBase64 = (imageSrc) => {
+  //   console.log(imageSrc);
+  // }
 
 
   return (<>
